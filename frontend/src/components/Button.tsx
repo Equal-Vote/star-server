6ba788965ebd7727a0b67f0683b4e396d27acd23
mikefranze <mikefranze@gmail.com>
2021-12-23 15:54:27 -0800

    Converting main app to typescript

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { MouseEventHandler } from 'react'
+
+type Props = {
+    onClick: MouseEventHandler,
+    color: string,
+    text: string,
+  }
+
+const Button = ({onClick,color,text}:Props) => {
+    return <button onClick = {onClick} style={{backgroundColor: color}} className='btn'> {text}</button>
+}
+
+Button.defaultProps = {
+    color:'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onclick: PropTypes.func
+}
+
+export default Button
