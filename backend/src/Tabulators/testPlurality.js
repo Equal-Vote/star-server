01969c6a00755394e1b15bbcf963a3c3f267974d
mikefranze <41272412+mikefranze@users.noreply.github.com>
2022-04-29 17:38:53 -1000

    Adding Approval and Plurality Tabulators and test scripts
    Test scripts aren't part of jest test framework yet, just outputs results to check formatting.

---
+++ backend/src/Tabulators/testPlurality.js
@@ -0,0 +1,15 @@
+const PluralityResults = require('./PluralityResults')
+
+const candidates = ['Alice','Bob','Carol','Dave']
+
+const votes = [
+    [0,0,0,1],
+    [1,0,0,0],
+    [1,0,0,0],
+    [0,0,0,1],
+    [0,0,0,1],
+]
+
+
+const results = PluralityResults(candidates,votes)
+console.log(results)
