ff08a349481735d2ff0f21c9a9e62b5a86389744
Arend Peter Castelein <apc1993@gmail.com>
2022-06-27 11:14:36 -0700

    Revert useFetch

--- frontend/src/useFetch.js
+++ frontend/src/useFetch.js
@@ -1,10 +1,33 @@
 import { useEffect, useState } from "react";
-import useFetchSetData from "./useFetchSetData";
 
-export const useFetch = (url,options) => {
-   const {data, setData, isPending, error} = useFetchSetData(url, options);
 
-   return {data, isPending, error};
+const useFetch = (url,options) => {
+    const [isPending,setIsPending] = useState(true)
+    const [error,setError] = useState(null)
+    const [data,setData] = useState(null)
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url,options)
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch data')
+                }
+                return res.json();
+            })
+            .then(data=> {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+        },1000);
+    },[url])
+    return {data, isPending, error}
 }
 
 export default useFetch;
