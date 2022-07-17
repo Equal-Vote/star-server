d346adbb4605ccacef584638f8c815097fae9220
scottplusplus <mail@scottplusplus.com>
2022-07-16 19:16:43 -0700

    adding TestHelper to keep things DRY / prevent API typos

--- backend/src/test/editElection.test.ts
+++ backend/src/test/editElection.test.ts
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const request = require('supertest');
 import { Election } from '../../../domain_model/Election';
-import makeApp from '../app';
-import { TestLoggerImpl } from '../Services/Logging/TestLoggerImpl';
+import { TestHelper } from './TestHelper';
 import testInputs from './testInputs';
 
-const app = makeApp()
+const th = new TestHelper();
 
 // Mocks databases for testing app
 // Uses mocks defined in ./../Models/__Mocks__/
@@ -13,65 +12,43 @@ jest.mock('./../Models/Ballots')
 jest.mock('./../Models/Elections')
 jest.mock('./../Models/ElectionRolls')
 
-var logger = new TestLoggerImpl().setup();
 
 afterEach(() => {
     jest.clearAllMocks();
-    logger.print();
-    logger.clear();
+    th.afterEach();
 });
 
+const setupInitialElection = async () => {
+    const response = await th.createElection(testInputs.Election1, [], testInputs.user1token);
+    expect(response.statusCode).toBe(200);
+    return response.election.election_id;
+}
+
+const fetchElectionById = async (electionId:number):Promise<Election> => {
+    const response = await th.fetchElectionById(electionId, testInputs.user1token);
+    expect(response.election).toBeTruthy();
+    return response.election;
+}
+
 describe("Edit Election", () => {
-
-    const setupInitialElection = async () => {
-        const response = await request(app)
-            .post('/API/Elections')
-            .set('Cookie', ['id_token=' + testInputs.user1token])
-            .set('Accept', 'application/json')
-            .send({ Election: testInputs.Election1, VoterIDList: [] })
-            .expect('Content-Type', /json/)
-        const responseObject =  Object.assign({}, response.body);
-        expect(responseObject).toHaveProperty("election.election_id");
-        return responseObject.election.election_id
-    }
-
-    const fetchElectionById = async (electionId:number):Promise<Election> => {
-        const response = await request(app)
-        .get(`/API/Election/${electionId}`)
-        .set('Cookie', ['id_token=' + testInputs.user1token])
-        .set('Accept', 'application/json')
-        expect(response.statusCode).toBe(200);
-
-        var resObj = response.body;
-        expect(resObj.election).toBeTruthy();
-        return resObj.election;
-    }
 
     describe("Election data provided", () => {
         test("responds with 200 status", async () => {
             const electionId = await setupInitialElection();
             console.log("electionId = "+electionId);
 
-            const response = await request(app)
-                .post(`/API/Election/${electionId}/edit`)
-                .set('Cookie', ['id_token=' + testInputs.user1token])
-                .set('Accept', 'application/json')
-                .send({ Election: testInputs.Election1, VoterIDList: [] })
+            const response = await th.editElection(testInputs.Election1, [], testInputs.user1token);
             expect(response.statusCode).toBe(200);
-            logger.clear();
+            th.testComplete();
         })
     })
 
     describe("Election not provided/incorrect format", () => {
         test("responds with 400 status", async () => {
             const ID = await setupInitialElection()
-
-            const response = await request(app)
-                .post(`/API/Election/${ID}/edit`)
-                .set('Cookie', ['id_token=' + testInputs.user1token])
-                .set('Accept', 'application/json')
-                .send({ VoterIDList: [] })
-            expect(response.statusCode).toBe(400)
+            const response = await th.postRequest(`/API/Election/${ID}/edit`, { VoterIDList: [] }, testInputs.user1token );
+            expect(response.statusCode).toBe(400);
+            th.testComplete();
         })
     })
 
@@ -79,28 +56,18 @@ describe("Edit Election", () => {
         test("responds with 401 status", async () => {
             const ID = await setupInitialElection()
 
-            const response = await request(app)
-                .post(`/API/Election/${ID}/edit`)
-                .set('Accept', 'application/json')
-                .send({ Election: testInputs.Election1, VoterIDList: [] })
-
-            expect(response.statusCode).toBe(401)
-            logger.clear();
+            const response = await th.createElection(testInputs.Election1, [], null);
+            expect(response.statusCode).toBe(401);
+            th.testComplete();
         })
     })
 
     describe("User is not owner", () => {
         test("responds with 401 status", async () => {
             const ID = await setupInitialElection()
-
-            const response = await request(app)
-                .post(`/API/Election/${ID}/edit`)
-                .set('Cookie', ['id_token=' + testInputs.user2token])
-                .set('Accept', 'application/json')
-                .send({ Election: testInputs.Election1, VoterIDList: [] })
-
-            expect(response.statusCode).toBe(401)
-            logger.clear();
+            const response = await th.editElection(testInputs.Election1, [], testInputs.user2token);
+            expect(response.statusCode).toBe(401);
+            th.testComplete();
         })
     })
 
@@ -113,18 +80,14 @@ describe("Edit Election", () => {
             election1Copy.election_id = electionId;
             election1Copy.title = newTitle;
 
-            const response = await request(app)
-                .post(`/API/Election/${electionId}/edit`)
-                .set('Cookie', ['id_token=' + testInputs.user1token])
-                .set('Accept', 'application/json')
-                .send({ Election: election1Copy, VoterIDList: [] })
+            const response = await th.editElection(election1Copy, [], testInputs.user1token);
 
             // expect(ElectionsDB.elections[election1Copy.election_id].title).toBe(newTitle)
             expect(response.statusCode).toBe(200);
 
             const reFetchedElection = await fetchElectionById(electionId);;
             expect(reFetchedElection.title).toEqual(newTitle);
-            logger.clear();
+            th.testComplete();
         })
 
         test("edits roll type", async () => {
@@ -136,16 +99,12 @@ describe("Edit Election", () => {
             election1Copy.settings.election_roll_type = newRollType;
             election1Copy.election_id = ID;
 
-            const response = await request(app)
-                .post(`/API/Election/${ID}/edit`)
-                .set('Cookie', ['id_token=' + testInputs.user1token])
-                .set('Accept', 'application/json')
-                .send({ Election: election1Copy, VoterIDList: [] })
+            const response = await th.editElection(election1Copy, [], testInputs.user1token);
 
             // TODO: I couldn't figure out how to make this work, it kept saying that ElectionsDB.mock was undefined?
             // expect(ElectionsDB.mock.instances[0].elections[election1Copy.election_id].settings.election_roll_type).toBe(newRollType)
             expect(response.statusCode).toBe(200)
-            logger.clear();
+            th.testComplete();
         })
         test("edits voter ids", async () => {
             // TODO
