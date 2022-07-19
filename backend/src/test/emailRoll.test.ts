ad9b35d0d95d85757a8c912bd897c9d1d084c537
scottplusplus <mail@scottplusplus.com>
2022-07-19 07:14:23 -0700

    emailRoll cleaned

--- backend/src/test/emailRoll.test.ts
+++ backend/src/test/emailRoll.test.ts
@@ -1,23 +1,20 @@
-require('dotenv').config();
-const request = require('supertest');
-import { TestHelper } from './TestHelper';
-import testInputs from './testInputs';
-
+require("dotenv").config();
+const request = require("supertest");
+import { TestHelper } from "./TestHelper";
+import testInputs from "./testInputs";
 
 // Mocks databases for testing app
 // Uses mocks defined in ./../Models/__Mocks__/
-jest.mock('./../Models/Ballots')
-jest.mock('./../Models/Elections')
-jest.mock('./../Models/ElectionRolls')
+jest.mock("./../Models/Ballots");
+jest.mock("./../Models/Elections");
+jest.mock("./../Models/ElectionRolls");
 
 const th = new TestHelper();
-const app = th.expressApp;
 
 afterEach(() => {
     jest.clearAllMocks();
     th.afterEach();
 });
-
 
 describe("Email Roll", () => {
     beforeAll(() => {
@@ -25,69 +22,71 @@ describe("Email Roll", () => {
     });
     var electionId = 0;
     test("Create election, responds 200", async () => {
-        const response = await request(app)
-            .post('/API/Elections')
-            .set('Cookie', ['id_token=' + testInputs.user1token])
-            .set('Accept', 'application/json')
-            .send({ Election: testInputs.EmailRollElection, VoterIDList: testInputs.EmailRoll });
-        //console.log(response.body)
-        expect(response.statusCode).toBe(200)
-        const responseObject = response.body;
-        electionId = responseObject.election.election_id
-        th.testComplete();
-    })
-    test("Get voter auth, is authorized and hasn't voted", async () => {
-        const response = await request(app)
-            .post(`/API/Election/${electionId}/ballot`)
-            .set('Cookie', ['id_token=' + testInputs.user1token])
-            .set('Accept', 'application/json')
-        expect(response.statusCode).toBe(200)
-        expect(response.body.voterAuth.authorized_voter).toBe(true)
-        expect(response.body.voterAuth.has_voted).toBe(false)
-        th.testComplete();
-    })
-    test("Authorized voter submits ballot", async () => {
-      const response = await th.submitBallot(
-        electionId,
-        testInputs.Ballot1,
-        testInputs.user1token
-      );
+        const response = await th.createElection(
+            testInputs.EmailRollElection,
+            testInputs.EmailRoll,
+            testInputs.user1token
+        );
 
-      // console.log(response)
-      expect(response.statusCode).toBe(200);
-      th.testComplete();
+        expect(response.statusCode).toBe(200);
+        electionId = response.election.election_id;
+        th.testComplete();
+    });
+    test("Get voter auth, is authorized and hasn't voted", async () => {
+        const response = await th.requestBallot(
+            electionId,
+            testInputs.user1token
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.voterAuth.authorized_voter).toBe(true);
+        expect(response.voterAuth.has_voted).toBe(false);
+        th.testComplete();
+    });
+    test("Authorized voter submits ballot", async () => {
+        const response = await th.submitBallot(
+            electionId,
+            testInputs.Ballot1,
+            testInputs.user1token
+        );
+
+        // console.log(response)
+        expect(response.statusCode).toBe(200);
+        th.testComplete();
     });
     test("Get voter auth, is authorized and has voted", async () => {
-      const response = await request(app)
-        .post(`/API/Election/${electionId}/ballot`)
-        .set("Cookie", ["id_token=" + testInputs.user1token])
-        .set("Accept", "application/json");
-      expect(response.statusCode).toBe(200);
-      expect(response.body.voterAuth.authorized_voter).toBe(true);
-      expect(response.body.voterAuth.has_voted).toBe(true);
-      th.testComplete();
+        const response = await th.requestBallot(
+            electionId,
+            testInputs.user1token
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.voterAuth.authorized_voter).toBe(true);
+        expect(response.voterAuth.has_voted).toBe(true);
+        th.testComplete();
     });
     test("Authorized voter re-submits ballot", async () => {
-      const response = await th.submitBallot(
-        electionId,
-        testInputs.Ballot1,
-        testInputs.user1token
-      );
-      // console.log(response)
-      expect(response.statusCode).toBe(400);
-      th.testComplete();
+        const response = await th.submitBallot(
+            electionId,
+            testInputs.Ballot1,
+            testInputs.user1token
+        );
+        // console.log(response)
+        expect(response.statusCode).toBe(400);
+        th.testComplete();
     });
 
     test("Get voter auth, isn't authorized and hasn't voted", async () => {
-        const response = await request(app)
-            .post(`/API/Election/${electionId}/ballot`)
-            .set('Cookie', ['id_token=' + testInputs.user3token])
-            .set('Accept', 'application/json')
-        expect(response.statusCode).toBe(200)
-        expect(response.body.voterAuth.authorized_voter).toBe(false)
-        expect(response.body.voterAuth.has_voted).toBe(false)
+        const response = await th.requestBallot(
+            electionId,
+            testInputs.user3token
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.voterAuth.authorized_voter).toBe(false);
+        expect(response.voterAuth.has_voted).toBe(false);
         th.testComplete();
-    })
+    });
     test("Unauthorized voter submits ballot", async () => {
         const response = await th.submitBallot(
             electionId,
@@ -95,7 +94,7 @@ describe("Email Roll", () => {
             testInputs.user3token
         );
         // console.log(response)
-        expect(response.statusCode).toBe(400)
+        expect(response.statusCode).toBe(400);
         th.testComplete();
-    })
-})
+    });
+});
