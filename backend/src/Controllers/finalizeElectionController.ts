007a6dc5f4e656aab0bbcfe16a493632912ea533
eznarf <41272412+eznarf@users.noreply.github.com>
2022-08-23 21:33:24 -0700

    Moves check for email election roll to before state change

--- backend/src/Controllers/finalizeElectionController.ts
+++ backend/src/Controllers/finalizeElectionController.ts
@@ -4,6 +4,7 @@ import { permissions } from '../../../domain_model/permissions';
 import { expectPermission } from "./controllerUtils";
 import { BadRequest, InternalServerError } from "@curveball/http-errors";
 import { Invites } from "../Services/Email/EmailTemplates"
+import { ElectionRoll } from '../../../domain_model/ElectionRoll';
 
 var ElectionsModel = ServiceLocator.electionsDb();
 var ElectionRollModel = ServiceLocator.electionRollDb();
@@ -19,6 +20,18 @@ const finalizeElection = async (req: any, res: any, next: any) => {
         Logger.info(req, msg);
         throw new BadRequest(msg)
     }
+
+    const electionId = req.election.election_id;
+    let electionRoll:ElectionRoll[]|null = null
+    if (req.election.settings.election_roll_type === 'Email') {
+        electionRoll = await ElectionRollModel.getRollsByElectionID(electionId, req);
+        if (!electionRoll) {
+            const msg = `Election roll for ${electionId} not found`;
+            Logger.info(req, msg);
+            throw new BadRequest(msg)
+        }
+    }
+
     var failMsg = "Failed to update Election";
     req.election.state = 'finalized'
     const updatedElection = await ElectionsModel.updateElection(req.election, req, `Finalizing election`);
@@ -28,17 +41,10 @@ const finalizeElection = async (req: any, res: any, next: any) => {
     }
 
     if (updatedElection.settings.election_roll_type === 'Email') {
-        const electionId = updatedElection.election_id;
-        const electionRoll = await ElectionRollModel.getRollsByElectionID(electionId, req);
-        if (!electionRoll) {
-            const msg = `Election roll for ${electionId} not found`;
-            Logger.info(req, msg);
-            throw new BadRequest(msg)
-        }
         Logger.info(req, `${className}.sendInvitations`, { election_id: updatedElection.election_id });
         try {
             const url = req.protocol + '://' + req.get('host')
-            const invites = Invites(updatedElection, electionRoll, url)
+            const invites = Invites(updatedElection, electionRoll as ElectionRoll[], url)
             EmailService.sendEmails(invites)
         } catch (err: any) {
             const msg = `Could not send invitations`;
