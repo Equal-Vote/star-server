29c62281111b38d3278dc7e360710c8fc2e90fa6
eznarf <41272412+eznarf@users.noreply.github.com>
2022-10-06 19:56:30 -0700

    Adding multi race to election form

--- frontend/src/components/ElectionForm.tsx
+++ frontend/src/components/ElectionForm.tsx
@@ -34,6 +34,8 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
             description: '',
             races: [
                 {
+                    title: '',
+                    description: '',
                     num_winners: 1,
                     voting_method: 'STAR',
                     candidates: [] as Candidate[],
@@ -56,7 +58,9 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
     const [titleError, setTitleError] = useState(false)
     const [newCandidateName, setNewCandidateName] = useState('')
     const [expandedSettings, setExpandedSettings] = useState(false)
-
+    const [openRace, setOpenRace] = useState(0)
+    
+    console.log(election)
     const applyElectionUpdate = (updateFunc) => {
         const electionCopy = structuredClone(election)
         updateFunc(electionCopy)
@@ -98,14 +102,6 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
             frontend_url: '', // base URL for the frontend
             owner_id: authSession.getIdField('sub'),
             state: 'draft',
-            races: [
-                {
-                    ...election.races[0],
-                    race_id: '0',
-                    title: election.title,
-                    description: election.description,
-                }
-            ]
         }
 
         try {
@@ -115,11 +111,11 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
         }
     }
 
-    const onAddCandidate = () => {
+    const onAddCandidate = (race_index) => {
         applyElectionUpdate(election => {
-            election.races[0].candidates.push(
+            election.races[race_index].candidates?.push(
                 {
-                    candidate_id: String(election.races[0].candidates.length),
+                    candidate_id: String(election.races[race_index].candidates.length),
                     candidate_name: newCandidateName, // short mnemonic for the candidate
                     full_name: '',
                 }
@@ -128,9 +124,22 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
         setNewCandidateName('')
     }
 
-    const onSaveCandidate = (candidate: Candidate, index) => {
+    const onAddRace = () => {
         applyElectionUpdate(election => {
-            election.races[0].candidates[index] = candidate
+            election.races.push(
+                    {   
+                        race_id: String(election.races.length),
+                        num_winners: 1,
+                        voting_method: 'STAR',
+                        candidates: [] as Candidate[],
+                    }
+            )
+        })
+    }
+
+    const onSaveCandidate = (race_index, candidate: Candidate, index) => {
+        applyElectionUpdate(election => {
+            election.races[race_index].candidates[index] = candidate
         })
     }
     const onUpdateElectionRoll = (voterRoll: string) => {
@@ -348,73 +357,130 @@ const ElectionForm = ({authSession, onSubmitElection, prevElectionData, submitTe
                     </div>}
                     <Divider light />
                     <Typography gutterBottom variant="h6" component="h6">Race Settings</Typography>
-                    <Grid item>
-                        <Box sx={{ minWidth: 120 }}>
-                            <FormControl fullWidth>
-                                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                                    Voting Method
-                                </InputLabel>
-                                <Select
-                                    name="Voting Method"
-                                    label="Voting Method"
-                                    value={election.races[0].voting_method}
-                                    onChange={(e) => applyElectionUpdate(election => {election.races[0].voting_method = e.target.value})}
-                                >
-                                    <MenuItem key="STAR" value="STAR">
-                                        STAR
-                                    </MenuItem>
-                                    <MenuItem key="STAR-PR" value="STAR-PR">
-                                        STAR-PR
-                                    </MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Box>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            id="num-winners"
-                            name="Number Of Winners"
-                            label="Number of Winners"
-                            inputProps={getStyle('races', 0, 'num_winners')}
-                            type="number"
-                            value={election.races[0].num_winners}
-                            onChange={(e) => applyElectionUpdate(election => {election.races[0].num_winners = e.target.value})}
-                        />
-                    </Grid>
-                    <Divider light />
-                    <Typography gutterBottom variant="h6" component="h6">
-                        Candidates
-                    </Typography>
-                    {election.races[0].candidates.map((candidate, index) => (
-                        <Grid item sm={12}>
-                            <AddCandidate onSaveCandidate={(newCandidate) => onSaveCandidate(newCandidate, index)} candidate={candidate} index={index} />
-                            <Divider light />
+                    {election.races?.map((race, race_index) => (
+                        <>
+                        {election.races.length>1 && 
+                            <Grid container alignItems="center">
+                            <Grid item sm={11}>
+                                <Typography gutterBottom variant="h6" component="h6">{`Race ${race_index+1}`}</Typography>
+                            </Grid>
+                            {!(openRace===race_index) &&
+                                    <Grid item sm={1}>
+                                        <IconButton aria-label="Home" onClick={() => { setOpenRace(race_index) }}>
+                                            <ExpandMore />
+                                        </IconButton>
+                                    </Grid>}
+                            {openRace===race_index &&
+                                <Grid item sm={1}>
+                                <IconButton aria-label="Home" onClick={() => { setOpenRace(-1) }}>
+                                    <ExpandLess />
+                                </IconButton>
+                                </Grid>}
+                            </Grid>
+                            }
+                        
+                        {openRace===race_index &&
+                            <>  
+                            {election.races.length>1 &&
+                            <>
+                                <Grid item>
+                                <TextField
+                                    id={`race-title-${String(race_index)}`}
+                                    name="title"
+                                    label="Title"
+                                    inputProps={getStyle('description')}
+                                    type="text"
+                                    value={election.races[race_index].title}
+                                    onChange={(e) => applyElectionUpdate(election => {election.races[race_index].title = e.target.value})}
+                                    />
+                                </Grid>
+                                 
+                                <Grid item>
+                                <TextField
+                                    id={`race-description-${String(race_index)}`}
+                                    name="description"
+                                    label="Description"
+                                    inputProps={getStyle('description')}
+                                    multiline
+                                    type="text"
+                                    value={election.races[race_index].description}
+                                    onChange={(e) => applyElectionUpdate(election => {election.races[race_index].description = e.target.value})}
+                                    />
+                                </Grid>
+                                </>}
+                            <Grid item>
+                            <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth>
+                                    <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                                        Voting Method
+                                    </InputLabel>
+                                    <Select
+                                        name="Voting Method"
+                                        label="Voting Method"
+                                        value={election.races[race_index].voting_method}
+                                        onChange={(e) => applyElectionUpdate(election => {election.races[race_index].voting_method = e.target.value})}
+                                    >
+                                        <MenuItem key="STAR" value="STAR">
+                                            STAR
+                                        </MenuItem>
+                                        <MenuItem key="STAR-PR" value="STAR-PR">
+                                            STAR-PR
+                                        </MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
                         </Grid>
+                        <Grid item>
+                            <TextField
+                                id= {`num-winners-${String(race_index)}`}
+                                name="Number Of Winners"
+                                label="Number of Winners"
+                                inputProps={getStyle('races', 0, 'num_winners')}
+                                type="number"
+                                value={election.races[race_index].num_winners}
+                                onChange={(e) => applyElectionUpdate(election => {election.races[race_index].num_winners = e.target.value})}
+                            />
+                        </Grid>
+                        <Divider light />
+                        <Typography gutterBottom variant="h6" component="h6">
+                            Candidates
+                        </Typography>
+                        {election.races[race_index].candidates?.map((candidate, index) => (
+                            <Grid item sm={12}>
+                                <AddCandidate onSaveCandidate={(newCandidate) => onSaveCandidate(race_index,newCandidate, index)} candidate={candidate} index={index} />
+                                <Divider light />
+                            </Grid>
+                        ))}
+                        <Grid container>
+                        <Grid item sm={8}>
+                            <TextField
+                                id={`new-candidate-name-${String(race_index)}`}
+                                name="new-candidate-name"
+                                label="New Candidate Name"
+                                type="text"
+                                value={newCandidateName}
+                                fullWidth
+                                onChange={(e) => {
+                                    setNewCandidateName(e.target.value)
+                                }}
+                                onKeyPress={(ev) => {
+                                    if (ev.key === 'Enter') {
+                                        onAddCandidate(race_index)
+                                        ev.preventDefault();
+                                    }
+                                }}
+                            />
+                        </Grid>
+                        <Grid item sm={4}>
+                        <Button variant='outlined' onClick={() => onAddCandidate(race_index)} >Add</Button>
+                        </Grid>
+                        </Grid>
+                        </>}
+                    <Divider light />
+                        </>
                     ))}
-                    <Grid container>
-                    <Grid item sm={8}>
-                        <TextField
-                            id="new-candidate-name"
-                            name="new-candidate-name"
-                            label="New Candidate Name"
-                            type="text"
-                            value={newCandidateName}
-                            fullWidth
-                            onChange={(e) => {
-                                setNewCandidateName(e.target.value)
-                            }}
-                            onKeyPress={(ev) => {
-                                if (ev.key === 'Enter') {
-                                    onAddCandidate()
-                                    ev.preventDefault();
-                                }
-                              }}
-                        />
-                    </Grid>
-                    <Grid item sm={4}>
-                    <Button variant='outlined' onClick={() => onAddCandidate()} >Add</Button>
-                    </Grid>
-                    </Grid>
+                    
+                    <Button variant='outlined' onClick={() => onAddRace()} >Add Race</Button>
                     <input 
                         type='submit' 
                         value={submitText} 
