45b7e470a20cadfd77ad5ec1994018849aa6a8b9
eznarf <41272412+eznarf@users.noreply.github.com>
2022-10-16 19:56:29 -0700

    One ballot per page

--- frontend/src/components/Election/Voting/VotePage.tsx
+++ frontend/src/components/Election/Voting/VotePage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import StarBallot from "./StarBallot";
+import BallotSelector from "./BallotSelector";
 import useFetch from "../../../useFetch";
 import { useParams } from "react-router";
 import React from 'react'
@@ -7,27 +7,30 @@ import { useNavigate } from "react-router";
 import { Ballot } from "../../../../../domain_model/Ballot";
 import { Vote } from "../../../../../domain_model/Vote";
 import { Score } from "../../../../../domain_model/Score";
-import { Container } from "@material-ui/core";
+import { Box, Container } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-const VotePage = ({ election, fetchElection}) => {
+const VotePage = ({ election, fetchElection }) => {
   const { id } = useParams();
-  const [rankings, setRankings] = useState([])
-  const navigate = useNavigate();
+  const [scores, setScores] = useState(election.races.map((race) =>
+    Array(race.candidates.length).fill(null)))
 
+  const navigate = useNavigate();
+  const [currentRace, setCurrentRace] = useState(0)
   const { data, isPending, error, makeRequest: postBallot } = useFetch(`/API/Election/${id}/vote`, 'post')
-  const onUpdate = (race_index, newRaceRankings) => {
-    var newRankings = [...rankings]
-    newRankings[race_index] = newRaceRankings
-    setRankings(newRankings)
+  const onUpdate = (race_index, newRaceScores) => {
+    var newRankings = [...scores]
+    newRankings[race_index] = newRaceScores
+    setScores(newRankings)
   }
   const submit = async () => {
-    const votes: Vote[] = 
+    const votes: Vote[] =
       election.races.map((race, race_index) => (
-      {race_id: '0',
-      scores: election.races[race_index].candidates.map((candidate, i) =>
-        ({ 'candidate_id': election.races[race_index].candidates[i].candidate_id, 'score': rankings[race_index][i] } as Score)
-      )
-      }))
+        {
+          race_id: '0',
+          scores: election.races[race_index].candidates.map((candidate, i) =>
+            ({ 'candidate_id': election.races[race_index].candidates[i].candidate_id, 'score': scores[race_index][i]===null ? 0 : scores[race_index][i] } as Score)
+          )
+        }))
 
     const ballot: Ballot = {
       ballot_id: '0', //Defaults to zero but is assigned ballot id by server when submitted
@@ -43,22 +46,43 @@ const VotePage = ({ election, fetchElection}) => {
     await fetchElection() // refetch election to update voter auth
     navigate(`/Election/${id}`)
   }
+  console.log(scores)
   return (
     <Container disableGutters={true} maxWidth="sm">
-      {election.races.map((race, race_index)=> (
-      <StarBallot
-        race={election.races[race_index]}
-        candidates={election.races[race_index].candidates}
-        onUpdate={newRankings => {onUpdate(race_index, newRankings)}}
-        defaultRankings={Array(election.races[race_index].candidates.length).fill(0)}
+      <BallotSelector
+        race={election.races[currentRace]}
+        candidates={election.races[currentRace].candidates}
+        onUpdate={newRankings => { onUpdate(currentRace, newRankings) }}
+        scores={scores[currentRace]}
       />
-      ))}
-      <Button
-        variant='outlined'
-        onClick={submit}
-        disabled={isPending}>
-        Submit Ballot
-      </Button>
+      {election.races.length > 1 &&
+        <Box sx={{ display: 'flex', justifyContent: "space-between" }}>
+          <Button
+            variant='outlined'
+            onClick={() => setCurrentRace(count => count - 1)}
+            disabled={currentRace === 0}
+            style={{ minWidth:"150px" }}>
+            Previous Race
+          </Button>
+          <Button
+            variant='outlined'
+            onClick={() => setCurrentRace(count => count + 1)}
+            disabled={currentRace === election.races.length-1}
+            style={{ minWidth:"150px" }}>
+            Next Race
+          </Button>
+        </Box>
+      }
+      
+      <Box sx={{ display: 'flex', justifyContent: "space-between" }}>
+        <Button
+          variant='outlined'
+          onClick={submit}
+          disabled={isPending||currentRace !== election.races.length-1 || scores[currentRace].every(score => score===null)}//disable unless on last page and at least one candidate scored
+          style={{ marginLeft: "auto", minWidth:"150px" }}>
+          Submit Ballot
+        </Button>
+      </Box>
       {<div> {error} </div>}
       {isPending && <div> Submitting... </div>}
     </Container>
