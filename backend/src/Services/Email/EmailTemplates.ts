32b5df2b26b0561f004cf56c74b7e76fd6793418
eznarf <41272412+eznarf@users.noreply.github.com>
2023-04-23 14:21:34 -0700

    Fixing unique email invites

--- backend/src/Services/Email/EmailTemplates.ts
+++ backend/src/Services/Email/EmailTemplates.ts
@@ -9,7 +9,7 @@ export function Invites(election: Election, voters: ElectionRoll[], url: string)
         from: 'elections@star.vote', // Change to your verified sender
         subject: `Invitation to Vote In ${election.title}`,
         text: `You have been invited to vote in ${election.title} ${url}/Election/${election.election_id}`,
-        html: `<div> <h3> You have been invited to vote in ${election.title}</h3> <a clicktracking="off" href="${url}/Election/${election.election_id}" >Link to Election</a></div>`,
+        html: `<div> <h3> You have been invited to vote in ${election.title}</h3> <a clicktracking="off" href="${url}/Election/${election.election_id}/${election.settings.voter_authentication.voter_id === true ? 'id/' + voter.voter_id : ''}" >Link to Election</a></div>`,
     })
 }
 
