829c3fdf3c1b7e98c1e17430f568eea1224dd778
Arend Peter Castelein <apc1993@gmail.com>
2023-04-29 20:40:34 -0700

    Tweak colors to match latest ballot style

--- frontend/src/components/Election/Voting/GenericBallotView.js
+++ frontend/src/components/Election/Voting/GenericBallotView.js
@@ -6,7 +6,7 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import {FaStar} from 'react-icons/fa';
+import {FaRegStar} from 'react-icons/fa';
 import { Link } from "@mui/material";
 import Box from '@mui/material/Box';
 
@@ -51,7 +51,7 @@ const Row = ({ rowIndex, candidate, score, onClick, columns }) => {
   const hasExpandedData = HasExpandedData(candidate)
   var rowColor = 'white'
   if (rowIndex % 2 == 0) {
-    rowColor = '#EDEDED';
+    rowColor = '#E3EDEF';
   } else {
     rowColor = 'white';
   }
@@ -182,7 +182,7 @@ const ColumnHeadings = ({starHeadings, columns, leftTitle, rightTitle, headingPr
 
 const ScoreIcon = ({color, value}) => (
   <div align='center' style={{width: '50px', height: '50px'}}>
-    <FaStar style={{color: color}} className="starIcon"/>
+    <FaRegStar style={{color: color}} className="starIcon"/>
     <Typography className="scoreColumnHeading">
       {value}
     </Typography>
@@ -194,7 +194,7 @@ const ScoreColumnHeadings = ({starHeadings, columns}) =>
     <Grid item xs={1}>
       { starHeadings &&
         <ScoreIcon
-          color={(n != 0)? '#CCCCCC' : '#FFFFFF'}
+          color={(n != 0)? '#B7D2D6' : '#FFFFFF'}
           value={columnTitle}
         />
       }
