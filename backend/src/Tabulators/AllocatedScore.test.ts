a2e4e5f1b0957a3f4df1e04dee70b0b298b8a56c
eznarf <41272412+eznarf@users.noreply.github.com>
2023-05-31 21:27:28 -0700

    Adding extra test case for fractional surplus

--- backend/src/Tabulators/AllocatedScore.test.ts
+++ backend/src/Tabulators/AllocatedScore.test.ts
@@ -28,14 +28,14 @@ describe("Allocated Score Tests", () => {
         // Voters who gave Allison their highest score have their ballot weight reduced to (1-6/8) = 0.25
         const candidates = ['Allison', 'Bill', 'Carmen', 'Doug']
         const votes = [
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 5, 1, 0],
-            [5, 4, 4, 0],
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0.25 after first round
+            [5, 4, 4, 0],// Ballot weight reduced to 0.25 after first round
             [0, 0, 0, 3],
             [0, 0, 4, 5],
             [0, 0, 4, 5],
@@ -46,6 +46,33 @@ describe("Allocated Score Tests", () => {
         expect(results.elected[1].name).toBe('Doug');
         expect(results.summaryData.weightedScoresByRound[0]).toStrictEqual([40, 39, 23, 18]);  
         expect(results.summaryData.weightedScoresByRound[1]).toStrictEqual([0, 9.75, 14.75, 18]);
+    })
+
+    test("Fractional surplus on lower split", () => {
+        // Two winners, two main parties, Allison wins first round with highest score, Allison has 8 highest level supporters, more than the quota of 6 voters
+        // 4 gave Allison 5s, 4 gave Allison 4s. The 5's contribute 100% of their ballot weight to the quota bring the quota to 4/6 filled
+        // 4 who gave Allison 4s share remaining 2/6 quota equally, reducing their ballot weight to 0.5
+        const candidates = ['Allison', 'Bill', 'Carmen', 'Doug']
+        const votes = [
+            [4, 4, 1, 0],// Ballot weight reduced to 0.50 after first round
+            [4, 4, 1, 0],// Ballot weight reduced to 0.50 after first round
+            [4, 4, 1, 0],// Ballot weight reduced to 0.50 after first round
+            [4, 4, 1, 0],// Ballot weight reduced to 0.50 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0 after first round
+            [5, 5, 1, 0],// Ballot weight reduced to 0 after first round
+            [5, 4, 4, 0],// Ballot weight reduced to 0 after first round
+            [0, 0, 0, 3],
+            [0, 0, 4, 5],
+            [0, 0, 4, 5],
+            [0, 0, 4, 5]]
+        const results = AllocatedScore(candidates, votes, 2, false, false)
+        console.log(results.summaryData.weightedScoresByRound)
+        expect(results.elected.length).toBe(2);
+        expect(results.elected[0].name).toBe('Allison');
+        expect(results.elected[1].name).toBe('Doug');
+        expect(results.summaryData.weightedScoresByRound[0]).toStrictEqual([36, 35, 23, 18]);  
+        expect(results.summaryData.weightedScoresByRound[1]).toStrictEqual([0, 8, 14, 18]);
     })
 
     test("Random Tiebreaker", () => {
