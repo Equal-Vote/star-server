880945398b07f0129194608ae32ed6f0dab1d07f
eznarf <41272412+eznarf@users.noreply.github.com>
2023-06-09 10:46:38 -0700

    Adding footer template

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter, GitHub } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.grey[200],//Make theme pallete for this
+        p: 6,
+        width: '100%'
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              About Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Some stuff about us
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              123 Main Street, Anytown, USA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: info@example.com
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +1 234 567 8901
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/STARVoting" color="inherit">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/starvoting/"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://twitter.com/5starvoting" color="inherit">
+              <Twitter />
+            </Link>
+            <Link href="https://github.com/Equal-Vote" color="inherit">
+              <GitHub />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://your-website.com/">
+              Your Website
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
