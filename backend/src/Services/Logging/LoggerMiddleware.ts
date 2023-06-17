835c62f9561bfedf06cc0487ca3dee7190bde187
eznarf <41272412+eznarf@users.noreply.github.com>
2023-06-17 13:18:06 -0700

    Logger reorder

--- backend/src/Services/Logging/LoggerMiddleware.ts
+++ backend/src/Services/Logging/LoggerMiddleware.ts
@@ -1,8 +1,8 @@
 import { IRequest } from "../../IRequest";
 import Logger from "./Logger";
 
-export function loggerMiddleware(req:IRequest, res:any, next:any):void {
-    Logger.info(req, `\nREQUEST: ${req.method} ${req.url} @ ${new Date(Date.now()).toISOString()} ip:${req.ip}`);
+export function loggerMiddleware(req: IRequest, res: any, next: any): void {
+    Logger.info({ contextId: req.contextId, logPrefix: '\n' }, `\nREQUEST: ${req.method} ${req.url} @ ${new Date(Date.now()).toISOString()} ip:${req.ip}`);
 
     res.on('finish', () => {
         Logger.info(req, `RES: ${req.method} ${req.url}  status:${res.statusCode}`);
