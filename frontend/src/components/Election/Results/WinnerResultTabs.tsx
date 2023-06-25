99f7536c676155c2cc7db43d7359bbfba483e8bd
Arend Peter Castelein <apc1993@gmail.com>
2023-06-25 07:31:33 -0700

    More variable renames

--- frontend/src/components/Election/Results/WinnerResultTabs.tsx
+++ frontend/src/components/Election/Results/WinnerResultTabs.tsx
@@ -2,27 +2,27 @@ import React, {useState} from 'react'
 import Typography from '@mui/material/Typography';
 import {Paper, Box} from '@mui/material';
 
-const WinnerResultTabs = ({children, rounds}) => {
+const WinnerResultTabs = ({children, num_winners}) => {
     const [currentTab, setCurrentTab] = useState(0);
 
-    if(rounds == 1) return <>{children}</>
+    if(num_winners == 1) return <>{children}</>
 
     let i = 0;
-    const roundIndexes = Array.from({length: rounds}, () => i++);
+    const roundIndexes = Array.from({length: num_winners}, () => i++);
 
     return <>
-        {rounds >= 5 && 
-            <Typography variant="h6">Rounds</Typography>
+        {num_winners >= 5 && 
+            <Typography variant="h6">num_winners</Typography>
         }
         <Box display="flex" flexDirection="row" gap={0}>
             {roundIndexes.map((i) => (
-                <div className={(i == currentTab)? 'roundResultsTab activeRoundResultsTab' : 'roundResultsTab'} onClick={() => setCurrentTab(i)}>
-                    {rounds < 5 && 'Round'} {i+1}
+                <div className={(i == currentTab)? 'winnerResultTab activewinnerResultTab' : 'winnerResultTab'} onClick={() => setCurrentTab(i)}>
+                    {num_winners < 5 && 'Round'} {i+1}
                 </div> 
             ))}
         </Box>
         {children.map((child, i) => (
-            <div className={(i == currentTab)? 'roundResults activeRoundResults' : 'roundResults'}>
+            <div className={(i == currentTab)? 'winnerResult activeWinnerResults' : 'winnerResult'}>
                 {child}
             </div>
         ))}
