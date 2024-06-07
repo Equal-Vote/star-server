ef76dc12c5c1dda4c588dcd8611d99b5aee5c42c
Evans Tucker <gmail@evanstucker.com>
2024-06-06 20:15:02 -0700

    Setting up React to use env vars on launch, and optimizing docker build

---
+++ replace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in $(env | grep ^REACT_APP_); do
+  key=$(echo $i | cut -d '=' -f 1)
+  value=$(echo $i | cut -d '=' -f 2-)
+  find /usr/src/app/packages/frontend -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|__${key}__|${value}|g" '{}' \+
+done
