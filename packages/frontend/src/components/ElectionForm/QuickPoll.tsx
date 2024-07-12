4b58b0fd929a012596153c11daa50f9c7d6873dd
eznarf <41272412+eznarf@users.noreply.github.com>
2024-07-11 22:27:48 -0700

    Updating Quick Poll default authentication to voter_id

--- packages/frontend/src/components/ElectionForm/QuickPoll.tsx
+++ packages/frontend/src/components/ElectionForm/QuickPoll.tsx
@@ -55,7 +55,7 @@ const QuickPoll = ({ authSession, methodName, grow }) => {
         settings: {
             voter_access: 'open',
             voter_authentication: {
-                ip_address: true,
+                voter_id: true,
             },
             ballot_updates: false,
             public_results: true,
