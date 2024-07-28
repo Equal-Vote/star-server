c601ac27894fb17309690c3378ebba1eeb25e1e7
Arend Peter <apc1993@gmail.com>
2024-07-27 17:59:13 -0700

    Tab out app.ts

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -18,46 +18,46 @@ const asyncHandler = require('express-async-handler')
 
 export default function makeApp() {
 
-const app = express();
-const appInitContext = Logger.createContext("appInit");
-const server = socketHandler(app);
+    const app = express();
+    const appInitContext = Logger.createContext("appInit");
+    const server = socketHandler(app);
 
-// CORS (Cross-origin resource sharing), allows for the backend to receive calls from the front end, even though they have different urls/origins
-//      (at least that's my understanding)
-const prodEndpoints : any = process.env.ALLOWED_URLS?.split(',') || 'https://dev.star.vote/';
-app.use(cors({
-    origin: prodEndpoints,
-    credentials: true, // allow the backend to receive cookies from the frontend
-}))
+    // CORS (Cross-origin resource sharing), allows for the backend to receive calls from the front end, even though they have different urls/origins
+    //      (at least that's my understanding)
+    const prodEndpoints : any = process.env.ALLOWED_URLS?.split(',') || 'https://dev.star.vote/';
+    app.use(cors({
+        origin: prodEndpoints,
+        credentials: true, // allow the backend to receive cookies from the frontend
+    }))
 
-// Set to trust proxy so we can resolve client IP address
-app.enable('trust proxy')
+    // Set to trust proxy so we can resolve client IP address
+    app.enable('trust proxy')
 
-app.use(iRequestMiddleware);
-app.use(loggerMiddleware);
+    app.use(iRequestMiddleware);
+    app.use(loggerMiddleware);
 
-app.use(cookieParser())
+    app.use(cookieParser())
 
-const frontendPath = '../../../../packages/frontend/build/';
+    const frontendPath = '../../../../packages/frontend/build/';
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, frontendPath)));
-app.use(express.json());
-//Routes
-app.use('/API',authController.getUser, electionRouter)
-// app.use('/debug',debugRouter)
+    const path = require('path');
+    app.use(express.static(path.join(__dirname, frontendPath)));
+    app.use(express.json());
+    //Routes
+    app.use('/API',authController.getUser, electionRouter)
+    // app.use('/debug',debugRouter)
 
-app.post('/API/Token', asyncHandler(getUserToken));
+    app.post('/API/Token', asyncHandler(getUserToken));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, frontendPath + "index.html"));
-})
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, frontendPath + "index.html"));
+    })
 
-app.use(errorCatch);
+    app.use(errorCatch);
 
-registerEvents();
+    registerEvents();
 
-Logger.debug(appInitContext, "app Init complete");
+    Logger.debug(appInitContext, "app Init complete");
 
-return server;
+    return server;
 }
