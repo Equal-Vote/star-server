c22832879212d398af868f3d2e5293af9d132482
Arend Peter <apc1993@gmail.com>
2024-07-27 18:09:28 -0700

    Move new vote flow to the backend

--- packages/backend/src/socketHandler.ts
+++ packages/backend/src/socketHandler.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import { Server } from 'socket.io';
 
 const { innerGetGlobalElectionStats} = require('./Controllers/getElectionsController')
 
-export default (app: express.Application) => {
-    const server = require('http').createServer(app)
-    const {Server} = require('socket.io')
 
-    const io = new Server(server, {
+export let io: Server|null = null;
+
+export const setupSockets = (app: express.Application) => {
+    const server = require('http').createServer(app)
+
+    io = new Server(server, {
         cors: {
             origin: process.env.ALLOWED_URLS?.split(',') || 'localhost'
         }
@@ -17,16 +20,7 @@ export default (app: express.Application) => {
             socket.join('landing_page');
             socket.emit('updated_stats', await innerGetGlobalElectionStats());
         })
-
-        socket.on('new_vote', (socket: any) => {
-            // use timeout so that there's time for the database to have the new vote applied
-            setTimeout(async () => {
-                io.to('landing_page').emit('updated_stats', await innerGetGlobalElectionStats());
-            }, 1000)
-        })
     })
-
-    
 
     return server;
 }
