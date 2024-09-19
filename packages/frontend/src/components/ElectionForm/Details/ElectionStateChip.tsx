cb16e763f57b37e499266dae09f14fdcae1cc830
Samuel Coleman <samuel.e.c519@gmail.com>
2024-09-19 16:37:54 -0600

    made the table filter for election status use chips

--- packages/frontend/src/components/ElectionForm/Details/ElectionStateChip.tsx
+++ packages/frontend/src/components/ElectionForm/Details/ElectionStateChip.tsx
@@ -4,35 +4,38 @@ import { Chip, Typography } from '@mui/material';
 import { useMemo } from 'react';
 import { useSubstitutedTranslation } from '~/components/util';
 
+const getStateColor = (state: ElectionState | "") => {
+  switch (state) {
+    case 'draft':
+      return 'purple';
+    case 'finalized':
+      return 'green';
+    case 'open':
+      return 'blue';
+    case 'closed':
+      return 'red';
+    case 'archived':
+      return 'gray4';
+    default:
+      return 'gray4';
+  }
+}
+
+export const makeChipStyle = (state: ElectionState | "") => {
+  const chipColor = getStateColor(state);
+  return {
+    backgroundColor: `brand.${chipColor}Transparent20`,
+    color: `brand.${chipColor}`,
+    borderColor: `brand.${chipColor}`,
+    // marginX: 2,
+  }
+}
+
+
 export default function ElectionStateChip({ state }: { state: ElectionState }) {
   const {t} = useSubstitutedTranslation();
 
-  const chipColor = useMemo(() => {
-    switch (state) {
-        case 'draft':
-            return 'purple';
-        case 'finalized':
-            return 'green';
-        case 'open':
-            return 'blue';
-        case 'closed':
-            return 'red';
-        case 'archived':
-            return 'gray4';
-        }
-    }
-    , [state]);
-    const chipStyle = useMemo(() => {
-        return {
-            backgroundColor: `brand.${chipColor}Transparent20`,
-            color: `brand.${chipColor}`,
-            borderColor: `brand.${chipColor}`,
-            marginX: 2,
-        }
-    }
-
-    , [chipColor]);
-
+  const chipStyle = useMemo(() => makeChipStyle(state), [state]);
 
   return (
     <Chip
