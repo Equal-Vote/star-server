c5477b7cddb0d2f39ee146e789e903d9a31b8044
Arend Peter <apc1993@gmail.com>
2024-09-21 01:06:58 -0700

    Add border

--- packages/frontend/src/components/styles.tsx
+++ packages/frontend/src/components/styles.tsx
@@ -30,8 +30,8 @@ export const Tip = (props: {name: TipName, electionTermType: TermType | undefine
                 tooltip: {
                     sx: {
                         background: '#2B344AFF', 
-                        //border: '2px solid white',
-                        boxShadow: '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
+                        border: '2px solid white',
+                        //boxShadow: '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
                     }
                 }
             }}
