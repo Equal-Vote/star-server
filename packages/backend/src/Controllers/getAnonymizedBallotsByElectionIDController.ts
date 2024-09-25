d7c957f64ac66cb758687aed196f4343c16e3a7c
Samuel Coleman <samuel.e.c519@gmail.com>
2024-09-24 18:04:49 -0600

    update anonymized ballots

--- packages/backend/src/Controllers/getAnonymizedBallotsByElectionIDController.ts
+++ packages/backend/src/Controllers/getAnonymizedBallotsByElectionIDController.ts
@@ -24,11 +24,16 @@ const getAnonymizedBallotsByElectionID = async (req: IElectionRequest, res: Resp
         Logger.info(req, msg);
         throw new BadRequest(msg)
     }
-    const anonymizedBallots: AnonymizedBallot[] = ballots.map((ballot) => {
-        return {
-            ballot_id: ballot.ballot_id,
-            votes: ballot.votes
-        }
+    const anonymizedBallots: AnonymizedBallot[] = ballots.filter(ballot => {
+        return ballot.status === "submitted" && ballot.head;
+    }).map((ballot) => {
+            return {
+                ballot_id: ballot.ballot_id,
+                election_id: ballot.election_id,
+                precinct: ballot.precinct,
+                votes: ballot.votes
+            }
+      
     });
     Logger.debug(req, "ballots = ", ballots);
     res.json({ ballots: anonymizedBallots })
