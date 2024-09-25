ee6a91c3cde5403f7b93c26f68fbae9d9f0c86e1
Samuel Coleman <samuel.e.c519@gmail.com>
2024-09-25 13:04:50 -0600

    added ElectionRoll Validation

--- packages/shared/src/domain_model/Util.ts
+++ packages/shared/src/domain_model/Util.ts
@@ -1,8 +1,8 @@
 export const checkForDuplicates = (inputArray: string[]): boolean => {
     return new Set(inputArray).size !== inputArray.length;
   }
-
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//from https://emailregex.com/
+export const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export const urlRegex = /^(http|https):\/\/[^ "]+$/;
 export const timeZones = [
