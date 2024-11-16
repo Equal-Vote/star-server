772e09b3ff850cff8c13ac8fc8dab64106da65e9
eznarf <41272412+eznarf@users.noreply.github.com>
2024-11-16 13:39:33 -0800

    setting default precint FF to false

--- packages/shared/src/config/SharedConfig.ts
+++ packages/shared/src/config/SharedConfig.ts
@@ -19,5 +19,5 @@ export const sharedConfig = {
     FF_CUSTOM_REGISTRATION: 'false',
     FF_VOTER_FLAGGING: 'false',
     FF_ELECTION_TESTIMONIALS: 'false',
-    FF_PRECINCTS: 'true',
+    FF_PRECINCTS: 'false',
 };
