836655b36399174c7667360b52a0f0a9bb75c0db
eznarf <41272412+eznarf@users.noreply.github.com>
2025-03-09 00:14:50 -0800

    Add write-in support to results processing

--- packages/backend/src/Controllers/Election/getElectionResultsController.ts
+++ packages/backend/src/Controllers/Election/getElectionResultsController.ts
@@ -31,17 +31,35 @@ const getElectionResults = async (req: IElectionRequest, res: Response, next: Ne
     const election = req.election
     let results: ElectionResults[] = []
     for (let race_index = 0; race_index < election.races.length; race_index++) {
-        const candidateNames = election.races[race_index].candidates.map((Candidate: any) => (Candidate.candidate_name))
-        const race_id = election.races[race_index].race_id
+        const race = election.races[race_index]
+        const candidateNames = race.candidates.map((Candidate: any) => (Candidate.candidate_name))
+        const candidateIDs = race.candidates.map((Candidate: any) => (Candidate.candidate_id))
+        const useWriteIns = race.enable_write_in && race.write_in_candidates && race.write_in_candidates.length>0
+        const writeInCandidates = useWriteIns && race.write_in_candidates ? race.write_in_candidates : [] 
+        const writeInCandidateNames = useWriteIns && race.write_in_candidates ? race.write_in_candidates.map(candidate => candidate.candidate_name) : []
+        const number_of_candidates = candidateNames.length + writeInCandidateNames.length
+        const race_id = race.race_id
         const cvr: ballot[] = []
-        const num_winners = election.races[race_index].num_winners
-        const voting_method = election.races[race_index].voting_method
+        const num_winners = race.num_winners
+        const voting_method = race.voting_method
+        const fullCandidateNames = [...candidateNames,...writeInCandidateNames]
+
         ballots.forEach((ballot: Ballot) => {
             const vote = ballot.votes.find((vote) => vote.race_id === race_id)
             if (vote) {
-                let row: ballot = vote.scores.map((score: Score) => (
-                    score.score
-                ))
+                let row: ballot = new Array(number_of_candidates).fill(null)
+                vote.scores.forEach(score => {
+                    const candidateIndex = candidateIDs.indexOf(score.candidate_id)
+                    if (candidateIndex > 0) {
+                        row[candidateIndex] = score.score
+                    }
+                    else if (candidateIndex == -1 && useWriteIns && score.write_in_name) {
+                        const write_in_name = score.write_in_name // typescript sees score.write_in_name as possibly undefined unless I extract it to another variable for some reason
+                        const writeInCandidateIndex = writeInCandidates.findIndex(candidate => candidate.aliases.includes(write_in_name))
+                        row[writeInCandidateIndex + candidateNames.length] = score.score
+                    }
+                })
+
                 // Feels hacky to add overrank information as an additional column
                 // but the other alternatives required updating the voting method inputs 
                 // and that would need refactors to all methods
@@ -58,10 +76,10 @@ const getElectionResults = async (req: IElectionRequest, res: Response, next: Ne
         const msg = `Tabulating results for ${voting_method} election`
         Logger.info(req, msg);
         let rng = seedrandom(election.election_id + ballots.length.toString())
-        const tieBreakOrders = election.races[race_index].candidates.map((Candidate) => (rng() as number))
+        const tieBreakOrders = fullCandidateNames.map((Candidate) => (rng() as number))
         results[race_index] = {
             votingMethod: voting_method,
-            ...VotingMethods[voting_method](candidateNames, cvr, num_winners, tieBreakOrders, election.settings)
+            ...VotingMethods[voting_method](fullCandidateNames, cvr, num_winners, tieBreakOrders, election.settings)
         }
     }
     
