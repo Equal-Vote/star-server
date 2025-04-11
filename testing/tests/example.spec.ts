6d97384b26f69adb869fce0709d4354d7f1f6d41
Samuel Coleman <samuel.e.c519@gmail.com>
2025-04-10 22:32:56 -0600

    fix test to succeed rather than fail on purpose

--- testing/tests/example.spec.ts
+++ testing/tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright9178/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
