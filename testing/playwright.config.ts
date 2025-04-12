097364944726a8d975cf455cb88d07594bcfe97a
Samuel Coleman <samuel.e.c519@gmail.com>
2025-04-12 01:13:40 -0600

    need to switch branches

--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  timeout: 10000,
+  timeout: 100000,
 
   /* Configure projects for major browsers */
   projects: [
